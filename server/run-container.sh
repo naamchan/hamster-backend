f5eb1232f8925567151e79ac3d05dd6ebb1532cf
Thara Mongkolwannalert <thara@hotplay.games>
2023-02-18 23:03:51 +0700

    - edit: deploy

--- server/run-container.sh
+++ server/run-container.sh
@@ -2,5 +2,5 @@
 
 dotnet publish --os linux --arch x64 -c Release -p:PublishProfile=DefaultContainer
 docker stop server
-dotnet ef database update
+docker cp hamster.db server:/app/hamster.db
 docker run -d --rm --name server -v ./db:/app/db -p 80:80 server:1.0.0 
